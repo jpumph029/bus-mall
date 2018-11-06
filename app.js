02cb2a56807fae865af4113936f86583bdf3a6be
Joe <pumphrey.joseph@gmail.com>
2018-11-06 15:02:20 -0800

    working getting unique images

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 'use strict';
 var allOptions = [];
 var threeOpt = [];
-var optionName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cuthulu', 'dog-duck', 'dragon', 'pren', 'pet-sweep', 'sissors', 'shark', 'sweep', 'taintain', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var optionName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 function Option(name, src) {
   this.name = name;
@@ -14,21 +14,39 @@ function Option(name, src) {
 // links option name with images and sticks the in the allOptions array
 (function buildOptions() {
   for (var i = 0; i < optionName.length; i++) {
-    new Option(optionName[i], 'assets/' + optionName[i] + '.jpg');  
+    new Option(optionName[i], 'assets/' + optionName[i] + '.jpg');
     console.log('you made all Options');
   }
 })();
 //generates 3 randoms numbers ------- TO DO: THESE ARENT UNIQUE YET
-(function getRandomOption() {
+function getRandomOption() {
   for (var i = 0; i < 3; i++) {
-    threeOpt.push(Math.floor(Math.random() * optionName.length));
+    threeOpt.push(Math.floor(Math.random() * optionName.length))
   };
-  return console.log('You generated 3 random options', threeOpt)
-}());
-// renders options from threeOpt
+  return console.log('You generated 3 random options', threeOpt);
 
-   document.getElementById('opt1').src = allOptions[threeOpt[0]].src;
-   document.getElementById('opt2').src = allOptions[threeOpt[1]].src;
-   document.getElementById('opt3').src = allOptions[threeOpt[2]].src;
+}
+getRandomOption();
 
 
+function isUnique() {
+if (threeOpt[0] !== threeOpt[1] && threeOpt[1] !== threeOpt[2] && threeOpt[2] !== threeOpt[0]) {
+  return true;
+} else {
+  return false;
+}
+}
+
+function getUniqueOptions() {
+if (isUnique() === true) {
+  var opt1 = document.getElementById('opt1').src = allOptions[threeOpt[0]].src;
+  var opt2 = document.getElementById('opt2').src = allOptions[threeOpt[1]].src;
+  var opt3 = document.getElementById('opt3').src = allOptions[threeOpt[2]].src;
+  return;
+} else{ 
+  threeOpt = [];
+  getRandomOption(); 
+  getUniqueOptions();
+} 
+}
+getUniqueOptions();
