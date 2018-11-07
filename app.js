320ffab568fc02b3c7a40d4ea605895a79edc849
Joe <pumphrey.joseph@gmail.com>
2018-11-07 14:47:10 -0800

    commiting to get on the right branch

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var opt1 = '';
 var opt2 = '';
 var opt3 = '';
 this.maxClicks = 0;
+var ctx = document.getElementById("resultsChart").getContext('2d');
 var optionName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 function Option(name, src) {
@@ -63,11 +64,6 @@ function drawOptions() {
   getUniqueOptions();
 }
 
-// makes the Options clickable
-document.getElementById('opt1').addEventListener('click', handlesUserClicks);
-document.getElementById('opt2').addEventListener('click', handlesUserClicks);
-document.getElementById('opt3').addEventListener('click', handlesUserClicks);
-
 function handlesUserClicks() {
   for (var i = 0; i < allOptions.length; i++) {
     if (allOptions[i].src === opt1 || allOptions[i].src === opt2 || allOptions[i].src === opt3) {
@@ -75,15 +71,64 @@ function handlesUserClicks() {
       maxClicks++;
     }
   }
-  if (maxClicks <= 25) {
+  if (maxClicks < 25) {
+    localStorage.setItem('voteData', JSON.stringify(allOptions));
     drawOptions();
   } else if (maxClicks === 25) {
-    // do something or nothing. 
+    displayChart();
+    document.getElementById('clearData').addEventListener('click', function () {
+      localStorage.clear();
+      window.location.reload();
+    });
   }
 }
-<<<<<<< HEAD
+// makes the Options clickable
+document.getElementById('opt1').addEventListener('click', handlesUserClicks);
+document.getElementById('opt2').addEventListener('click', handlesUserClicks);
+document.getElementById('opt3').addEventListener('click', handlesUserClicks);
+
 function displayChart() {
-  
+  var names = [];
+  for (var i = 0; i < allOptions.length; i++) {
+    names.push(allOptions[i].name);
+  }
+
+  var votes = [];
+  for (var j = 0; j < allOptions.length; j++) {
+    votes.push(allOptions[j].clicks);
+  }
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+      labels: names,
+      datasets: [{
+        label: "My First dataset",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: votes,
+      }]
+    },
+
+    // Configuration options go here
+    options: {
+      responsive: false,
+    }
+  });
 }
-=======
->>>>>>> a975498410342146c157845f0cb72228dbf4d2a7
+(function localStorageCheck() {
+  if (localStorage.voteData) {
+    console.log('There are items in localStorage');
+    var parsedOptions = JSON.parse(localStorage.voteData);
+    for (var i = 0; i < allOptions.length; i++) {
+      allOptions = parsedOptions;
+    }
+  }
+  else {
+    console.log('localStorage is empty');
+  }
+})();
+
+
